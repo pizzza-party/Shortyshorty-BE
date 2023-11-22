1a803cde8934b78ea0b3167c52cdcfebda1b2fc3
Sujin Jeon <mackerel0106@gmail.com>
2023-11-22 19:11:34 +0900

    Feat: swagger config 작성

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { StatusCodes } from 'http-status-codes';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
 import { indexToBase62, base62ToIndex } from './converter';
 
 dotenv.config({ path: '../.env.dev' });
@@ -9,6 +12,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+
+const file = fs.readFileSync('./src/swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.post('/api', (req, res) => {
   try {
